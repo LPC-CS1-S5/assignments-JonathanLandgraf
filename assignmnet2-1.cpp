b20ae068bbeeac67f78b48fc827bdbad29cd9dab
JonathanTL <jonathanlandgraf@gmail.com>
2021-01-25 17:47:14 +0000

    Finished assignment 2-1

---
+++ assignmnet2-1.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  //Declare variables
+  int checking; 
+  unsigned int miles;
+  long days;
+
+  //Set variables
+  checking = -20;
+  miles = 4276;
+  days = 18900;
+
+  //Output text
+  cout << "We have made a long journey of " << miles << " miles.\n";
+  cout << "Our checking account balance is " << checking;
+  cout << "\nAbout " << days << " days ago Columbus ";
+  cout << "stood on this spot.\n";
+  return 0;
+
+}

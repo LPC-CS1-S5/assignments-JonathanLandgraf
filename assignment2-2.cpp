ad0310ec98eb7af9e5d89a939ec010738c157291
JonathanTL <jonathanlandgraf@gmail.com>
2021-01-25 18:08:44 +0000

    created assignment2-2

---
+++ assignment2-2.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  char letter;
+  letter = 74;
+  cout << letter;
+
+  letter = 79;
+  cout << letter;
+
+  letter = 78;
+  cout << letter;
+
+  letter = 65;
+  cout << letter;
+
+  letter = 84;
+  cout << letter;
+
+  letter = 72;
+  cout << letter;
+
+  letter = 65;
+  cout << letter;
+
+  letter = 78;
+  cout << letter;
+  return 0;
+
+}

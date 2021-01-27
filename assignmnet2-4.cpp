7601c80472faed1af2688a36c655d30589617b68
JonathanTL <jonathanlandgraf@gmail.com>
2021-01-27 17:19:22 +0000

    Initial creation of assignment 2-4

---
+++ assignmnet2-4.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  //initialize all varaibles
+  float = regular_price = 59.95;
+  float = discount_amount = regular_price X 0.2;
+  float = sales_price = regular_price - discount_amount;
+
+
+  //display 
+  cout << "The regular price is $" << regular_price << endl;
+  cout << "Discount amount is $" << discount_amount << endl;
+  cout << "The sales price is $" << sales_price << endl;
+
+  return 0;
+
+}

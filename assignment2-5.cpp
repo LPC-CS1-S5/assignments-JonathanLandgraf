42613ad32a6ac2587ac6a38972f9972fcc775cb7
JonathanTL <jonathanlandgraf@gmail.com>
2021-01-27 17:52:28 +0000

    Slight changes

--- assignment2-5.cpp
+++ assignment2-5.cpp
@@ -7,13 +7,13 @@ int main()
   float meal_cost = 88.67;
   float tax = meal_cost * .0675;
   float tip = (meal_cost + tax) * .2;
-
+  double total_cost = meal_cost + tax + tip;
 
   //display 
   cout << "The meal costs $" << meal_cost << endl;
   cout << "The tax is $" << tax << endl;
   cout << "The tip is $" << tip << endl;
-  cout << "The total bill is $" << (meal_cost + tax + tip) << endl;
+  cout << "The total bill is $" << total_cost << endl;
 
 
   return 0;

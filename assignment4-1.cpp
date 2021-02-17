b4c7101780cdf41b914619290a12bbaf79e8c554
JonathanTL <jonathanlandgraf@gmail.com>
2021-02-17 05:49:18 +0000

    _

---
+++ assignment4-1.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <string>
+#include <iomanip>
+#include <cstdlib>
+#include <ctime>
+using namespace std;
+
+int main()
+{
+  //initialize all varaibles
+  const int d20 = 0.2;
+  const int d30 = 0.3;
+  const int d40 = 0.4;
+  const int d50 = 0.5;
+
+  int input = 0;
+  float Oprice, Dprice, Sprice = 0.0
+
+  if (input > 0 && input < 10){
+    
+
+  }
+
+  //display results
+  
+
+  return 0; 
+
+}

a355e1a82d4bb3b19bb3a94de3aad170162c61f5
JonathanTL <jonathanlandgraf@gmail.com>
2021-03-03 06:27:48 +0000

    Finished assignment 5-1

--- assignment5-1.cpp
+++ assignment5-1.cpp
@@ -6,23 +6,39 @@ using namespace std;
 
 int main()
 {
-  int n, num = 0;
+  int n, num, n2 = 0;     //num is not used for the assignment, but was used when doing and showing lab 5-7 work
   int nthpower = 1;
-  double invertpower = 0.0;
 
-  cout << "Please enter in an integer (positive or negative):  \n";
-  cin >> n;
+  double invertpower = 0.0; //Used only for the lab 5-7 work
+
+  bool inputCheck = false;
+
+
+  while (inputCheck == false){
+  cout << "Please enter 2 integers (between 0 and 1300):  \n";
+  cin >> n >> n2;
+  if (n >= 0  && n2 < 1300){
+    inputCheck = true;
+    }
+  }
+
+
+  /*
+  Code block for lab 5-7 when there was a negative number
   if (n < 0) {
     num = n * -1;
   }
   else if (n > 0) {
     num = n;
   }
+  */
 
-  for (int i = 0; i < num; i++){
+  while (n < nthpower && nthpower < n2){
     nthpower = nthpower * 2;
   }
 
+  /*
+  Code block part of lab 5-7 for when using a negative exponent
   if (n < 0) {
     invertpower = 1.0 / nthpower;
     cout << "The  power is : " << invertpower << endl;
@@ -30,7 +46,9 @@ int main()
   else {
     cout << "The power is : " << nthpower << endl;
   }
-
+  */
+  cout << "The input numbers are " << n << " and " << n2 << endl;
+  cout << "The power is : " << nthpower << endl;
 
   return 0;
 }

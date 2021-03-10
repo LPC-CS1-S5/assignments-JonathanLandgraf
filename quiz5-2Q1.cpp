973faa08fc8e71abbda58ea5cce4fb512a8203c8
JonathanTL <jonathanlandgraf@gmail.com>
2021-03-10 16:11:29 +0000

    Set up on Quiz 5-2 Question 1

---
+++ quiz5-2Q1.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+#include <ctime>
+using namespace std;
+
+int main()
+{
+  int num1, num2, num3, num4, num5;
+  srand(time(0));
+  rdnum = rand( ) ;  
+
+  num1 = rand() % 100;
+  num2 = rand() % 100;
+  num3 = rand() % 100;
+  num4 = rand() % 100;
+  num5 = rand() % 100;
+
+
+
+
+
+
+  return 0;
+}

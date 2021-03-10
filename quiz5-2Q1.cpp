130cc52d112441c39de7de527b20dae110e0d463
JonathanTL <jonathanlandgraf@gmail.com>
2021-03-10 16:14:59 +0000

    Confirm random numbers generated

--- quiz5-2Q1.cpp
+++ quiz5-2Q1.cpp
@@ -7,14 +7,15 @@ using namespace std;
 int main()
 {
   int num1, num2, num3, num4, num5;
-  srand(time(0));
-  rdnum = rand( ) ;  
+  srand(time(0));  
 
   num1 = rand() % 100;
   num2 = rand() % 100;
   num3 = rand() % 100;
   num4 = rand() % 100;
   num5 = rand() % 100;
+
+  cout << "The random numbers generated are: " << num1 << ", " << num2 << ", " << num3 << ", " << num4 << ", and " << num5 << endl;
 
 
 

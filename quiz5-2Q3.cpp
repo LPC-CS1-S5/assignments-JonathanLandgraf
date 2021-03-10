66b6442d4291c910599fbd8208e3ee435795b671
JonathanTL <jonathanlandgraf@gmail.com>
2021-03-10 18:10:31 +0000

    Update

--- quiz5-2Q3.cpp
+++ quiz5-2Q3.cpp
@@ -5,7 +5,12 @@ using namespace std;
 
 int main()
 {
+  int b, e;
 
+  do {
+    cout << "Please enter in the range to find the prime numbers: "
+    cin >> b >> e;
+  } while (e > 100 || b < 1);
     
 
 
--- quiz5-2Q4write.cpp
+++ quiz5-2Q4write.cpp
@@ -1,26 +1,58 @@
 #include <iostream>
 #include <iomanip>
 #include <fstream>
+#include <string>
 using namespace std;
 
 
 int main()
 {
   ifstream ifs;
-
+  int n, ID;
+  string name, dep;
+  double salery;
 
   ifs.open("employee.txt");
-  ifs (ifs.fail){
+  ifs (ifs.fail()) {
     cerr << "File failed to open\n";
     exit(0);
   }
 
-  
+  cout << "How many entries would you like to add? ";
+  cin >> n;
 
+  for (int i=1; i <= n;){
+    cout << "Information for employee #" << i << endl;
+		cout << "Please enter in the employee's ID number: ";
+		cin >> ID;
 
+		cout << "Please enter the name of the employee: ";
+		cin >> name;
 
+		cout << "Enter in the department name: ";
+		cin >> dep;
 
+		cout << "Enter the employee's salery: ";
+		cin >> salery;
 
+		// wrong operator, fix with >> 
+		ifs >> ID >> " ";
+		ifs >> name >> " ";
+		ifs >> dep >> " ";
+		ifs >> salery;
+    ifs >> endl;
 
+	// from line 22 to 39 
+	// must be repeated 
+	// How many times you want? 
+	// must be determined by "user input"
+	// if user says 5, we are going to make 5 iterations.
+
+    //I'll use a do for loop
+	// I think for-loop is better, becuase it has fixed number
+    i++;
+  }
+  ifs.close();
+  cout << "File written \n";
   return 0;
 }

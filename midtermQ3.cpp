d565b44233f1f40e64b4cd54aeeb8f1a8f3f5886
JonathanTL <jonathanlandgraf@gmail.com>
2021-03-31 16:16:01 +0000

    Question 3 for the Midterm

---
+++ midtermQ3.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+#include <fstream>
+#include <ctime>
+using namespace std;
+
+int getRdnum(void);
+int isGreater(int);
+
+int pre = -10;
+
+
+int main()
+{
+  ofstream ofs;
+  int num, N = 10;
+  srand(time(0));  
+
+
+  ofs.open("numbers.txt");
+  if (ofs.fail()) {
+    cerr << "File failed to open\n";
+    exit(0);
+  }
+
+  for (int i=0; i < N; i++){
+    num = getRdnum();
+    if (isGreater(num) == 1){
+      ofs << num;
+      ofs << "\t";
+    }
+  }
+
+
+
+  return 0;
+}
+
+int getRdnum()
+{
+  int num;
+  num = rand() % 50;
+  cout << num << endl;
+  return num;
+}
+
+int isGreater(int num)
+{
+  if (pre == -10){
+    pre = num;
+    return 0;
+  }
+  else if (pre < num){
+    pre = num;
+    return 1;
+  }
+  else
+    return 0;
+}

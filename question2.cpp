649d5e837caeb718b0c3c21cc2447b773a8c2da3
JonathanTL <>
2021-04-14 04:38:20 +0000

    Updating Quiz 6-1

--- question2.cpp
+++ question2.cpp
@@ -11,6 +11,8 @@ void printResult(int, int, int, int);
 
 int main()
 {
+
+  srand(time(0));
   int min;
 
   gerRandnum(int &n1, int &n2, int &n3);
@@ -18,4 +20,29 @@ int main()
   printResult(n1, n2, n3, min);
 
   return 0;
+}
+
+void gerRandnum(int &n1, int &n2, int &n3)
+{
+  n1 = rand() % 101;
+  n2 = rand() % 101;
+  n3 = rand() % 101;
+
+  return;
+}
+
+findMin(int num1, int num2, int num3)
+{
+  int min = num1;
+
+  if (min > num2)
+    min = num2;
+  if (min > num3)
+    min = num3;
+  return min;
+}
+
+printResult(n1, n2, n3, min)
+{
+  
 }

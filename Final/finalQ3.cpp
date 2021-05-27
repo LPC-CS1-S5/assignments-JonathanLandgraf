27999e160d8818c493ed5464de76b9bad5e25fc9
JonathanTL <>
2021-05-27 05:49:01 +0000

    Starting on Question 3

---
+++ Final/finalQ3.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <iomanip>
+
+int main()
+{
+  const int SIZE1=5;
+  const int SIZE2=10;
+  int numbers1[SIZE1] = {2, 3, 5, 9, 10};
+  int numbers2[SIZE2] = {1, 2, 3, 5, 9, 10, 11, 13, 15, 17};
+
+  for (int i = 0; i < SIZE2; i++) {
+    
+  }
+
+}

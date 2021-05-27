b17bf742542f143fdf8c66897c47bd73c1b454dc
JonathanTL <>
2021-05-27 05:51:56 +0000

    testing finalQ3.cpp

--- Final/finalQ3.cpp
+++ Final/finalQ3.cpp
@@ -7,9 +7,14 @@ int main()
   const int SIZE2=10;
   int numbers1[SIZE1] = {2, 3, 5, 9, 10};
   int numbers2[SIZE2] = {1, 2, 3, 5, 9, 10, 11, 13, 15, 17};
+  int j = 0;
 
   for (int i = 0; i < SIZE2; i++) {
-    
+    if (numbers2[i] == numbers1[])
+      j += 1;
   }
+
+  if (j == 5)
+    cout << "numbers1 is a subset of numbers2" << endl;
 
 }
